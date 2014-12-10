e6f55ec5ea9c84ddcf35966e07658b51a949f961
Josh Andrews <jtandr3ws37@gmail.com>
2014-12-10 00:31:21 -0800

    initialize user.js model, migrate pg.

---
+++ models/user.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define("user", {
+    email: DataTypes.STRING,
+    password_digest: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+
+  return user;
+};
